6ebdd3fb82254a2d6250dbc0ee93e9cae93a5151
dnuka <csx@tuta.io>
2018-08-09 11:28:42 +0530

    simple app working

--- securetube/securetube.py
+++ securetube/securetube.py
@@ -5,6 +5,7 @@ from pprint import pprint
 from bs4 import BeautifulSoup
 
 base = 'https://www.youtube.com/results/?search_query={}&disable_polymer=1&hl=en-GB&gl=US'
+results = {}
 
 
 def fetch(query):
@@ -12,7 +13,10 @@ def fetch(query):
 	req.add_header("User-Agent", "36435")
 	with request.urlopen(req) as youtube:
 		raw_data = BeautifulSoup(youtube, 'html.parser')
-	videos = raw_data.find_all('h3', attrs={'class': 'yt-lockup-title'})
-	return videos
-
-
+	#videos = raw_data.find_all('h3', attrs={'class': 'yt-lockup-title'})
+	videos = raw_data.findAll('a',attrs={'class':'yt-uix-tile-link'})
+	videoList = []
+	for video in videos:
+		tmp = 'https://www.youtube.com' + video['href']
+		videoList.append(tmp)
+	return videoList
