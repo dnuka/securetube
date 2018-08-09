da5ed292972488d9b927988fc02980c06aca97b3
dnuka <csx@tuta.io>
2018-08-09 12:04:07 +0530

    thumbnail fixed

--- securetube/securetube.py
+++ securetube/securetube.py
@@ -5,7 +5,11 @@ from pprint import pprint
 from bs4 import BeautifulSoup
 
 base = 'https://www.youtube.com/results/?search_query={}&disable_polymer=1&hl=en-GB&gl=US'
+
 results = {}
+
+def get_thumbnail(video_id):
+	return 'https://img.youtube.com/vi/{}/0.jpg'.format(video_id)
 
 
 def fetch(query):
