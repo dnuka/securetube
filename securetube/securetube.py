3997f2f4d9cfc531bccc5436b96da88d129c7d66
dnuka <csx@tuta.io>
2018-08-09 13:58:35 +0530

    app works fine. baisc video details works, including url description and thumbnail

--- securetube/securetube.py
+++ securetube/securetube.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 
 base = 'https://www.youtube.com/results/?search_query={}&disable_polymer=1&hl=en-GB&gl=US'
 
-results = {}
+results = []
 
 def get_thumbnail(video_id):
 	return 'https://img.youtube.com/vi/{}/0.jpg'.format(video_id)
@@ -14,13 +14,17 @@ def get_thumbnail(video_id):
 
 def fetch(query):
 	req = request.Request(base.format(query))
-	req.add_header("User-Agent", "36435")
+	req.add_header("User-Agent", "36438")
 	with request.urlopen(req) as youtube:
 		raw_data = BeautifulSoup(youtube, 'html.parser')
 	#videos = raw_data.find_all('h3', attrs={'class': 'yt-lockup-title'})
-	videos = raw_data.findAll('a',attrs={'class':'yt-uix-tile-link'})
-	videoList = []
-	for video in videos:
-		tmp = 'https://www.youtube.com' + video['href']
-		videoList.append(tmp)
-	return videoList
+	data = raw_data.findAll('a',attrs={'class':'yt-uix-tile-link'})
+	for info in data:
+		link = 'https://www.youtube.com' + info['href']
+		if '/watch?v' in link:
+			temp = {}
+			temp['url'] = link
+			temp['description'] = info['title']
+			temp['thumbnail'] = get_thumbnail(info['href'].strip('/watch?v='))
+			results.append(temp)
+	return results
